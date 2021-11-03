61bede5c657376335cd1f1c5daac3973967941ee
Joshua <joshua.t.strickland@gmail.com>
2021-11-02 21:42:57 -0400

    commit tree d0d1171b320e67827b997b50c81d95bd531e5a97

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`); 
+//   }
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(names, event){
+    let thankYouCards = []
+    for (let i=0; i < names.length; i++){
+        thankYouCards.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        
+    }
+    return thankYouCards
+    
+}
+console.log (writeCards(["Lisa", "Kaitlin", "Jane",], 'surprise'));
+
+function countDown (startingNumber){
+    while (startingNumber > -1) {
+      console.log (startingNumber--)
+    }
+    return countDown
+  }
+ countDown (20);
